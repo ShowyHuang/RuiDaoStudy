b9a54d0d47492e07ffe2b29516fd7eb61faac562
ShowyHuang <huangshengwei1993@outlook.com>
2016-12-08 18:42:24 +0800

    初见

---
+++ app/src/main/java/com/neusoft/ruidaostudy/baseinfo/ConnectionInfo.java
@@ -0,0 +1,15 @@
+package com.neusoft.ruidaostudy.baseinfo;
+
+/**
+ * Created by Administrator on 2016/12/8.
+ */
+public class ConnectionInfo {
+
+    public static String LoginUrl="http://www.neuedu.cn/m/mobileLogin!loginNeu.action";//登录
+    public static String ImageCodeUrl="http://www.neuedu.cn/imgcode";//图片验证码
+    public static String PositionUrl="http://www.neuedu.cn/m/mobilePostInfo!findPostInfo.action";//岗位列表信息
+    public static String AutoLoginUrl="http://www.neuedu.cn/m/mobileAutoLogin!autoLogin.action";//自动登录
+    public static String LogoutUrl="http://www.neuedu.cn/m/mobileLogout!logoutNeu.action";//退出登录
+    public static String PositionCourseUrl="http://www.neuedu.cn/m/mobilePostCourse!findPostCourse.action";//岗位关联课程列表
+    public static String CourseListInfo="http://www.neuedu.cn/m/mobileCourseInfo!findMoreMcourses.action";//课程列表信息
+}
---
+++ app/src/main/java/com/neusoft/ruidaostudy/baseinfo/Const.java
@@ -0,0 +1,18 @@
+package com.neusoft.ruidaostudy.baseinfo;
+
+/**
+ * Created by Administrator on 2016/12/8.
+ */
+public class Const {
+    public static final String NETWORK_ERROR = "无法连接到服务器！";
+    public static final String PARSE_ERROR = "数据格式错误！";
+    public static final String NO_CONNECTION_ERROR = "URL 格式错误！";
+    public static final String TIMEOUT_ERROR = "网络请求超时";
+    public static final String SERVER_ERROR = "服务器端错误，错误代码为：";
+
+    public Const() {
+
+
+    }
+
+}
